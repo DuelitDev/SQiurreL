16816379e3fe116b25dfc8ce20cbd83a0b720c69
DuelitDev <p@duelit.dev>
2026-01-08 19:55:05 +0900

    SQuirrel REPL WIP

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,24 @@ pub mod storage;
 pub mod var_char;
 
 use clap::Parser;
-
+use std::io::{self, Write};
+use tokio::fs;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long, default_value = "Guest")]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[arg(value_name = "DATABASE NAME")]
+    database: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello, {}!", args.name);
+    // 데이터베이스 유무 체크
+    if !fs::try_exists(&args.database).await.unwrap_or(false) {
+        panic!("Database '{}' does not exist.", args.database);
     }
+    println!("SQuirreL REPL (type '.exit' to stop)");
+    println!("DATABASE: {}!", args.database);
 }
-
-
