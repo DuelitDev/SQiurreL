eef29dc01e54f2a464ae7afe3c39bec36aaf3974
DuelitDev <jyoon07dev@gmail.com>
2026-03-23 11:04:13 +0900

    feat: implement default method for DataType enum to return corresponding DataValue

--- common/src/schema/mod.rs
+++ common/src/schema/mod.rs
@@ -7,6 +7,19 @@ pub enum DataType {
     Text,
 }
 
+impl DataType {
+    pub fn default(&self) -> DataValue {
+        match self {
+            DataType::Nil => DataValue::Nil,
+            DataType::Int => DataValue::Int(0),
+            DataType::Real => DataValue::Real(0.0),
+            DataType::Bool => DataValue::Bool(false),
+            DataType::Text => DataValue::Text(Box::from("")),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum DataValue {
     Nil,
     Int(i64),
